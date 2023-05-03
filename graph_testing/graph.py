c4049f6fd3f523be28516524c46fd12465705ff2
martires-colin <cmartires@nevada.unr.edu>
2023-05-02 20:10:11 -0700

    testing with networkx

--- graph_testing/graph.py
+++ graph_testing/graph.py
@@ -7,16 +7,27 @@ import networkx as nx
 # nx_graph = nx.davis_southern_women_graph()
 # nx_graph = nx.florentine_families_graph()
 # nx_graph = nx.les_miserables_graph()
-nx_graph = nx.random_internet_as_graph(20)
+nx_graph = nx.random_internet_as_graph(10, 1234)
 
 for i in range(0, len(nx_graph.nodes)):
     nx_graph.nodes[i]['title'] = str(i)
     nx_graph.nodes[i]['label'] = str(i)
 
+print(nx_graph.edges[(0,1)])
+
+# for i in range(0, len(nx_graph.edges)):
+#     nx_graph.edges[i]
+
+for edge in nx_graph.edges:
+    print(edge.data("weight"))
 
 
 nt = Network()
 
 nt.from_nx(nx_graph)
 
-nt.show("networkx.html", local=True, notebook=False)
+# nt.toggle_physics(False)
+# nt.toggle_drag_nodes(False)
+
+# nt.show("networkx.html", local=True, notebook=False)
+# nt.show("networkx2.html", local=True, notebook=False)
