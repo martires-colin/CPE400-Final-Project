a6fb83f6dd632187d40bcd77ab2ab4dabfcbb48d
Colin <cmartires@nevada.unr.edu>
2023-05-07 19:37:51 -0700

    more progress

--- test_controls.py
+++ test_controls.py
@@ -29,40 +29,21 @@ def main():
             elif userInput == "2":
                 rand_removal_of_node(G, color_map=color_map)
 
-            # Remove Selected Node
-            elif userInput == "3":
-                kill_node(G, color_map=color_map)
-
             # Display Graph
-            elif userInput == "4":
+            elif userInput == "3":
                 displayGraph(G, pos, color_map=color_map)
 
             # Calculate Shortest Path
-            elif userInput == "5":                
-                src = input("Source Node: ")
-                dest = input("Destination Node: ")
-                prev_nodes, shortest_paths = dijkstra_algorithm(G, src)
+            elif userInput == "4":                
+                # src = input("Source Node: ")
+                # dest = input("Destination Node: ")
+                dijkstra_algorithm(G, 7)
+                djikstra(G, 7, 5, pos)
 
-                try:               
-                    cur_node = int(dest)
-                    optimal_path = []
-                    while True:
-                        optimal_path.insert(0, cur_node)
-                        if cur_node == int(src):
-                            break
-                        cur_node = prev_nodes[cur_node]
-                        
-                    print(f'Optimal Path from Node {src} to Node {dest}: {optimal_path}')
-                    print(f'Optimal Path Cost from Node {src} to Node {dest}: {shortest_paths[int(dest)]}')
-                    path_edges = list(zip(optimal_path,optimal_path[1:]))
-
-                    displayGraph(G, pos, color_map=color_map, path_edges=path_edges)
-
-                except:
-                    print("\nPath not possible")
-
-                # djikstra(G, 7, 5, pos)
-
+            # Display Menu
+            elif userInput == "5":
+                displayMenu()
+            
             # Exit Program
             elif userInput == "6":
                 print("\nExiting Program")
@@ -83,9 +64,9 @@ def displayMenu():
 ---------------------------
 1 - Reset Graph
 2 - Remove Random Node
-3 - Remove Selected Node
-4 - Show Graph
-5 - Calculate Shortest Path
+3 - Show Graph
+4 - Calculate Shortest Path
+5 - Menu Options
 6 - Exit
 ---------------------------
         '''
@@ -116,6 +97,19 @@ def displayGraph(graph, pos, color_map, path_edges = []):
 # TODO
 # remove node based on some sort node attribute
 def rand_removal_of_node(graph, color_map):
+    # fail_number = round(rnd.random(), 2)
+    # print(list(graph.nodes(data=True)))
+
+    # for n in range(0,graph.number_of_nodes()):
+    #     print(graph[n])
+    #     if graph[n]["weight"] <= fail_number:
+    #          graph.remove_node(n)
+    #          print("removed node: " , n)
+    #          break
+            
+    # print(list(graph.nodes(data=True)))
+    # x = rnd.randint(0, graph.number_of_nodes() - 1)
+
     hasAliveNodes = False
     valid_nodes = []
     for node in graph.nodes:
@@ -141,71 +135,90 @@ def rand_removal_of_node(graph, color_map):
         print("All nodes are dead!")
         return
 
-def kill_node(graph, color_map):
-    hasAliveNodes = False
-    valid_nodes = []
-    for node in graph.nodes:
-        num_neighbors = graph.neighbors(node)
-        if len(list(num_neighbors)) != 0:
-            valid_nodes.append(node)
-            hasAliveNodes = True
 
-    if hasAliveNodes:
-        x = input("choose a node to kill: ")
-        rm_edges = list(graph.edges(int(x)))
-        for edge in rm_edges:
-            graph.remove_edge(edge[0], edge[1])
-        print("Killed node: " , x)
+# TODO
+# implement djikstra
+def djikstra(graph, start_node, end, pos):
+    path = nx.shortest_path(graph,source=start_node,target=end)
+    
+    
+    path_edges = list(zip(path,path[1:]))
+    nx.draw_networkx_nodes(graph, pos, node_size=700)
+    nx.draw_networkx_edges(graph,pos,edge_color='b',width=1)
+    nx.draw_networkx_edges(graph,pos,edgelist=path_edges,edge_color='r',width=5)
+    nx.draw_networkx_labels(graph, pos, font_size=20, font_family="sans-serif")
+    edge_labels = nx.get_edge_attributes(graph, "weight")
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels)
+    ax = plt.gca()
+    ax.margins(0.08)
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
 
-        # Change color of dead nodes
-        for node in graph.nodes:
-            num_neighbors = graph.neighbors(node)
-            if len(list(num_neighbors)) == 0:
-                color_map[node] = "red"
-        return x
-    else:
-        print("All nodes are dead!")
-        return
+    total_weight = 0
+    for e in path_edges:
+        print((graph[e[0]][e[1]]["weight"]))
+        total_weight += (graph[e[0]][e[1]]["weight"])
+    
+    print(f'Optimal Path: {path}')
+    print(f'Optimal Path Weight: {round(total_weight, 2)}')
+    
+
+
 
 def dijkstra_algorithm(graph, start_node):
-
-    output_path = []
+    # unvisited_nodes = list(graph.get_nodes())
     unvisited_nodes = list(graph.nodes())
+    print(f'Unvisited Nodes: {unvisited_nodes}')
+
+    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
     shortest_path = {}
+ 
+    # We'll use this dict to save the shortest known path to a node found so far
     previous_nodes = {}
  
-    
+    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
     max_value = 10
     for node in unvisited_nodes:
         shortest_path[node] = max_value
-   
+    # However, we initialize the starting node's value with 0   
     shortest_path[int(start_node)] = 0
     
-   
+    # The algorithm executes until we visit all nodes
     while unvisited_nodes:
-        
+        # The code block below finds the node with the lowest score
         current_min_node = None
-        
-        for node in unvisited_nodes: 
+        print(f'Current Min Node: {current_min_node}')
+        for node in unvisited_nodes: # Iterate over the nodes
             if current_min_node == None:
                 current_min_node = node
             elif shortest_path[node] < shortest_path[current_min_node]:
                 current_min_node = node
-        
-        neighbors = list(graph.neighbors(current_min_node))
-        
-        for neighbor in neighbors:
-           
-            tentative_value = shortest_path[current_min_node] + graph[current_min_node][neighbor]["weight"]
-            
-            if tentative_value < shortest_path[neighbor]:
-               
-                shortest_path[neighbor] = tentative_value
-                previous_nodes[neighbor] = current_min_node
-                output_path.append(neighbor)
-                
-        unvisited_nodes.remove(current_min_node)
+        print(f'Current Min Node: {current_min_node}')
 
+        # The code block below retrieves the current node's neighbors and updates their distances
+        # neighbors = graph.get_outgoing_edges(current_min_node)
+        neighbors = list(graph.neighbors(current_min_node))
+        print(f"{current_min_node}'s neighbors {neighbors}")
+
+        print(f'shortest paths: {shortest_path}')
+        for neighbor in neighbors:
+            # tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            tentative_value = shortest_path[current_min_node] + graph[current_min_node][neighbor]["weight"]
+            # print(f'Node []')
+            if tentative_value < shortest_path[neighbor]:
+                
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
+ 
+        # After visiting its neighbors, we mark the node as "visited"
+        unvisited_nodes.remove(current_min_node)
+        print(f'Unvisited Nodes: {unvisited_nodes}')
+
+    print(shortest_path)
+    print(previous_nodes)
+    
     return previous_nodes, shortest_path
 
 
